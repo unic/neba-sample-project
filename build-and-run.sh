e76579c49645f6942ec2db2f3f88852caa98be05
olaf-otto <olaf.otto@ormera.ch>
2021-04-08 21:25:26 +0200

    Travis CI setup

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,14 +3,14 @@ echo
 echo "Building sample app..."
 echo
 
-mvn clean install
+mvn clean install || exit 1
 
 echo
 echo "Building sling launchpad (this may take some time)..."
 echo
-pushd launchpad
-mvn clean install
-popd
+pushd launchpad || exit 1
+mvn clean install || exit 1
+popd || exit 1
 
 echo "Starting docker container. It will take some time for Sling to initialize."
-docker-compose up --force-recreate --remove-orphans
+docker-compose up --force-recreate --remove-orphans || exit 1

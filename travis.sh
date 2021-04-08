761366e711812d676ff35e5a60f32472491dcd4a
Olaf Otto <olaf.otto@unic.com>
2021-04-08 21:16:15 +0200

    Delete travis.sh

--- travis.sh
+++
@@ -1,9 +0,0 @@
-echo "Building sample app..."
-mvn clean install || exit 1
-
-echo
-echo "Building sling launchpad (this may take some time)..."
-echo
-pushd launchpad || exit 1
-mvn clean install || exit 1
-popd || exit 1

c02bca46e9baebecbf3434a89b83bd6d4a78abc6
olaf-otto <olaf.otto@ormera.ch>
2021-12-30 22:16:01 +0100

    Replaced travis with github actions

--- .github/workflows/ci.sh
+++ .github/workflows/ci.sh
@@ -2,13 +2,13 @@ echo
 echo "Building sample app..."
 echo
 
-mvn clean install || exit 1
+mvn -B clean install || exit 1
 
 echo
 echo "Building sling launchpad (this may take some time)..."
 echo
 pushd launchpad || exit 1
-mvn clean install || exit 1
+mvn -B clean install || exit 1
 popd || exit 1
 
 echo
